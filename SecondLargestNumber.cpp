8718aa5d27bdeba3a66ed28c51524af70c547b47
Jyotish <jyotishyadav09@gmail.com>
2025-03-29 05:05:24 +0530

    SecondLargestNumber

---
+++ SecondLargestNumber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+    int getSecondLargest(vector<int> arr){
+        int n= arr.size();
+        sort(arr.begin(),arr.end(),greater<int>());
+        for(int i=0;i<n;i++){
+            if(arr[i]<arr[0])
+            return arr[i];
+        }
+        return -1; //If above condition not satisfied then return -1
+    }
+}
