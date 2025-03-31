8f09d3ba815303c0d64e972715be83a2b0b87291
Jyotish <jyotishyadav09@gmail.com>
2025-03-31 17:49:47 +0530

    Days 2

--- PushZeroToEnd.cpp
+++ PushZeroToEnd.cpp
@@ -1,7 +1,7 @@
 class Solution {
     public:
       void pushZerosToEnd(vector<int>& arr) {
-          // code here
+          
           int count=0;
           for(int i=0;i<arr.size();i++){
               if(arr[i]!=0){
